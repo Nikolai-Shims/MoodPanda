232ee0df38c0d88aef3df8aafd63f0705819ffa0
Nikolai-Shims <Nikolai.Shims@gmail.com>
2020-09-28 00:30:14 +0300

    added allure

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,12 +1,16 @@
 package tests;
 
 import io.qameta.allure.Description;
+import io.qameta.allure.Issue;
+import io.qameta.allure.Link;
 import org.testng.annotations.Test;
 
 public class LoginTest extends BaseTest{
 
     @Test(description = "Login to Mood Panda")
     @Description("Open login page and field correct data")
+    @Issue("shim_nikolai")
+    @Link("moodpanda.com")
     public void loginToMoodPanda(){
         loginPage
                 .openloginPage()
